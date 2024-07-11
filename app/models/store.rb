8fa99b8bab26575fc5357f9d488ae898e986b508
AYUMUNISHI <a63yu3m6@gmail.com>
2024-07-11 15:12:41 +0900

    正規表現等修正

--- app/models/store.rb
+++ app/models/store.rb
@@ -9,12 +9,10 @@ class Store < ApplicationRecord
   belongs_to :lunch
   belongs_to :dinner
 
-  validates :phone, format: {with: /\A^\d[0-9]{3,4}[-][0-9]{4}[-][0-9]{4}\z/}
+  validates :phone, format: {with: /\A^0([0-9]-[0-9]{4}|[0-9]{2}-[0-9]{3}|[0-9]{3}-[0-9]{2}|[0-9]{4}-[0-9])-[0-9]{4}\z/}
 
   validates :images, :store, :product_description, :description, :cooking_name, :phone, :city, :block, :regular_day, presence: true
   validates :category_id, :prefecture_id, numericality: { other_than: 1 }
 
-  def save
-    store.create(store: store, product_description: product_description, description: description, category_id: category_id, cooking_name: cooking_name, lunch_id: lunch_id, dinner_id: dinner_id, phone: phone, prefecture_id: prefecture_id, city: city, block: block, building: "", regular_day: regular_day, user_id: user.id)
-  end
+
 end
