b26423b4bd2d19b821afb5e2571d8d04401ee4b8
AYUMUNISHI <a63yu3m6@gmail.com>
2024-07-12 18:09:24 +0900

    画像バリデーション

--- app/models/store.rb
+++ app/models/store.rb
@@ -9,10 +9,17 @@ class Store < ApplicationRecord
   belongs_to :lunch
   belongs_to :dinner
 
+  validate :must_have_three_images
   validates :phone, format: {with: /\A^0([0-9]-[0-9]{4}|[0-9]{2}-[0-9]{3}|[0-9]{3}-[0-9]{2}|[0-9]{4}-[0-9])-[0-9]{4}\z/}
 
   validates :images, :store, :product_description, :description, :cooking_name, :phone, :city, :block, :regular_day, presence: true
   validates :category_id, :prefecture_id, numericality: { other_than: 1 }
+
+  private
+  def must_have_three_images
+    errors.add(:images, "must have exactly three") if images.size != 4
+  end
+
 
 
 end
